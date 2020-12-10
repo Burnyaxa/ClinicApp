5fbe7a4a2ed168452b8efb75aa60c94eda0a4cfb
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-11 00:24:11 +0200

    Add Visit entity

---
+++ DAL/Entities/Visit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public enum VisitStatus
+    {
+        Awaiting,
+        Completed,
+        Cancelled,
+        Delayed
+    }
+
+    public class Visit
+    {
+        [ForeignKey("Patient")]
+        public int PatientId { get; set; }
+        public Patient Patient { get; set; }
+        [ForeignKey("Doctor")]
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+        public DateTime Date { get; set; }
+        [EnumDataType(typeof(VisitStatus))]
+        public VisitStatus Status { get; set; }
+        public string Diagnosis { get; set; }
+        public string Note { get; set; }
+    }
+}
