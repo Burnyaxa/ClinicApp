37e0c46d03d0a186cdcd2a2008cbebb89be790d7
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-10 23:39:16 +0200

    Add Doctor entity

---
+++ DAL/Entities/Doctor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class Doctor
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        public string Specialty { get; set; }
+
+    }
+}
