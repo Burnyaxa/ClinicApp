6379ba92b9649e804ee76dc732121878f513f239
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-10 23:34:52 +0200

    Add Patient entity

---
+++ DAL/Entities/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class Patient
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime BirthDate { get; set; }
+
+    }
+}
