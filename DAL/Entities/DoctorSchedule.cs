87295281ef42e39fcb713a743510cc38b4661fb3
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-11 00:10:55 +0200

    Add DoctorSchedule entity

---
+++ DAL/Entities/DoctorSchedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class DoctorSchedule
+    {
+        [ForeignKey("Doctor")]
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
