4edade822bc504961cca9b56323be34004bfb1b6
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-11 22:45:19 +0200

    Add context

---
+++ DAL/Data/ClinicDbContext.cs
@@ -0,0 +1,61 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Data
+{
+    public class ClinicDbContext : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Visit> Visits { get; set; }
+        public DbSet<DoctorSchedule> DoctorSchedules { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+
+        public ClinicDbContext() : base() { }
+
+        public ClinicDbContext(DbContextOptions<ClinicDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Doctor>()
+                .HasMany(x => x.Schedules)
+                .WithOne(x => x.Doctor)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasForeignKey(x => x.DoctorId)
+                .IsRequired();
+
+            modelBuilder.Entity<Patient>()
+                .HasMany(x => x.Visits)
+                .WithOne(x => x.Patient)
+                .HasForeignKey(x => x.PatientId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+
+            modelBuilder.Entity<Doctor>()
+                .HasMany(x => x.Visits)
+                .WithOne(x => x.Doctor)
+                .HasForeignKey(x => x.DoctorId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+
+            modelBuilder.Entity<Patient>()
+                .HasMany(x => x.Appointments)
+                .WithOne(x => x.Patient)
+                .HasForeignKey(x => x.PatientId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+
+            modelBuilder.Entity<Doctor>()
+                .HasMany(x => x.Appointments)
+                .WithOne(x => x.Doctor)
+                .HasForeignKey(x => x.DoctorId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+        }
+    }
+}
