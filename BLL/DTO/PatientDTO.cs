f6b30d7b312dbf4c6d4ca914a8eb2a4e73e862ad
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-13 16:52:41 +0200

    Add Patient DTO

---
+++ BLL/DTO/PatientDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.DTO
+{
+    class PatientDTO
+    {
+        public int Id { get; set; } 
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+
+        IEnumerable<> Visits { get; set; }
+        IEnumerable<> Appointments { get; set; }
+
+
+    }
+}
