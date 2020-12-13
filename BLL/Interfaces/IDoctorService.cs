78c108788bd59186111549f8e914449682589eab
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-13 21:20:22 +0200

    Add DoctorService interface

---
+++ BLL/Interfaces/IDoctorService.cs
@@ -0,0 +1,17 @@
+﻿using BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IDoctorService
+    {
+        Task<IEnumerable<DoctorDTO>> GetAllDoctors();
+        Task<DoctorDTO> GetDoctorById(int id);
+        Task<DoctorDTO> CreateDoctor(DoctorDTO patient);
+        Task<DoctorDTO> UpdateDoctor(int id, DoctorDTO patient);
+        Task DeleteDoctor(int id);
+    }
+}
