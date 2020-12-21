6f9fd8461fc7e0e6ef0b5fb14a5abd103ab37c0f
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-21 18:58:59 +0200

    Add VisitCreate model

---
+++ PL/Models/VisitCreateModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PL.Models
+{
+    public class VisitCreateModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int PatientId { get; set; }
+        [Required]
+        public int DoctorId { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+
+        public string Diagnosis { get; set; }
+
+        public string Note { get; set; }
+    }
+}
