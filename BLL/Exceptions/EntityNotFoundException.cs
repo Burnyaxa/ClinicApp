ca62ef756a905f8c58be34077b2f09fe7d84a475
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-22 00:41:31 +0200

    Add EntityNotFoundException

---
+++ BLL/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using BLL.Exceptions.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Exceptions
+{
+    public class EntityNotFoundException : NotFoundException
+    {
+        public override string Message { get; }
+
+        public EntityNotFoundException(string type, int id) : base()
+        {
+            Message = $"Couldn't find entity with type {type} and with id {id}";
+        }
+    }
+}
