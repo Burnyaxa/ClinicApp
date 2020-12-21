649835e432cccf6193440f40df62210e4e10f83d
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-21 18:57:26 +0200

    Add AppointmentUpdate model

---
+++ PL/Models/AppointmentUpdateModel.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PL.Models
+{
+    public class AppointmentUpdateModel
+    {
+        [Required]
+        public int PatientId { get; set; }
+        [Required]
+        public int DoctorId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public AppointmentStatus Status { get; set; }
+    }
+}
