ba733ed89b47df767104404dc29b0651caa0f2ff
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-21 17:53:02 +0200

    Add DoctorCreate model

---
+++ PL/Models/DoctorCreateModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PL.Models
+{
+    public class DoctorCreateModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string Patronymic { get; set; }
+        [Required]
+        public string Specialty { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public int Cabinet { get; set; }
+    }
+}
