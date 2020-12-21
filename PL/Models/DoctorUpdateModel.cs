8cb01df3d07ad0f78fc350741fad14ea9ccf6cc5
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-21 17:54:25 +0200

    Add DoctorUpdate model

---
+++ PL/Models/DoctorUpdateModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PL.Models
+{
+    public class DoctorUpdateModel
+    {
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public string Specialty { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public int Cabinet { get; set; }
+    }
+}
