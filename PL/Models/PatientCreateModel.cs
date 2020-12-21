34699f0140bc7d5a23a4781558467f0e173e810f
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-21 17:21:42 +0200

    Add PatientUpdate model and rename PatientCreate model

--- PL/Models/PatientCreateModel.cs
+++ PL/Models/PatientCreateModel.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 namespace PL.Models
 {
-    public class PatientCreatedModel
+    public class PatientCreateModel
     {
         [Required]
         public int Id { get; set; }
         [Required]
         public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
         [Required]
         public string Patronymic { get; set; }
         [Required]
