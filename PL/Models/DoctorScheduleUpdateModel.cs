0c368a644a06b5625083ede7b628620b15b35ce1
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-21 18:33:35 +0200

    Add DoctorScheduleUpdate model

---
+++ PL/Models/DoctorScheduleUpdateModel.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PL.Models
+{
+    public class DoctorScheduleUpdateModel
+    {
+        [Required]
+        public int DoctorId { get; set; }
+
+        public Days Day { get; set; }
+
+        public TimeSpan StartTime { get; set; }
+
+        public TimeSpan EndTime { get; set; }
+    }
+}
