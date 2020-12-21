87f3ab958d0bbaa6361526d1e2f6715575c985a2
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-21 17:19:00 +0200

    Add PatientCreated model

---
+++ PL/Models/PatientCreatedModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PL.Models
+{
+    public class PatientCreatedModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string Patronymic { get; set; }
+        [Required]
+        public DateTime BirthDate { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+}
