1ee2d95ab44b3733b4c3840629ef1fa2893229b7
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-22 16:43:40 +0200

    Add FreeTime controller

---
+++ PL/Controllers/FreeTimeController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FreeTimeController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IAppointmentTimeService _freeTimeService;
+
+        public FreeTimeController(IMapper mapper, IAppointmentTimeService freeTimeService)
+        {
+            _mapper = mapper;
+            _freeTimeService = freeTimeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetFreeTimeByDateAndDoctorId(DateTime date, int doctorId)
+        {
+            return Ok(await _freeTimeService.GetFreeHours(date, doctorId));
+        }
+    }
+
+}
