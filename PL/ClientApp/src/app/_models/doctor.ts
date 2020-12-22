b91c163d28bafc81c8b032636f40cefdf78f2c9c
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-22 17:16:24 +0200

    Add Doctor model

---
+++ PL/ClientApp/src/app/_models/doctor.ts
@@ -0,0 +1,10 @@
+export class Doctor {
+  id: number;
+  lastName: string;
+  firstName: string;
+  patronymic: string;
+  specialty: string;
+  address: string;
+  phoneNumber: string;
+  cabinet: number;
+}
